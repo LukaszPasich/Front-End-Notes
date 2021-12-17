754923afd56f01e24454c0e8ba9e453b1d6dfed5
LukaszPasich <lukas.zed81@gmail.com>
2021-12-17 18:27:12 +0000

    FEAT: Add collapsable submenu to CSS button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,26 @@
-$('#collapsable-navbar-html').hide();
-$('#collapsable-navbar-html-mobile').hide();
+// hide all sub-menus first
+$.fn.hideHtmlSubmenus = function(){ 
+    $('#collapsable-navbar-html').hide();
+    $('#collapsable-navbar-html-mobile').hide();
+}
 
+$.fn.hideCssSubmenus = function(){ 
+    $('#collapsable-navbar-css').hide();
+    $('#collapsable-navbar-css-mobile').hide();
+}
+
+$.fn.hideJsSubmenus = function(){ 
+    $('#collapsable-navbar-js').hide();
+    $('#collapsable-navbar-js-mobile').hide();
+}
+
+
+// html button
 $('#nav-button-html').on('click', function() {
+
+    $.fn.hideCssSubmenus();
+    $.fn.hideJsSubmenus();
+
     if ($(window).width() > 991.98) {
         if ( $('#collapsable-navbar-html').is(':hidden') ) {
             $('#collapsable-navbar-html').slideDown(200);
@@ -16,4 +35,28 @@ $('#nav-button-html').on('click', function() {
         }
 }});
 
+// css button
+$('#nav-button-css').on('click', function() {
 
+    $.fn.hideHtmlSubmenus();
+    $.fn.hideJsSubmenus();
+
+    if ($(window).width() > 991.98) {
+        if ( $('#collapsable-navbar-css').is(':hidden') ) {
+            $('#collapsable-navbar-css').slideDown(200);
+        } else {
+            $('#collapsable-navbar-css').hide(200);
+        }
+    } else {
+        if ( $('#collapsable-navbar-css-mobile').is(':hidden') ) {
+            $('#collapsable-navbar-css-mobile').slideDown(200);
+        } else {
+            $('#collapsable-navbar-css-mobile').hide(200);
+        }
+}});
+
+
+// innit
+$.fn.hideHtmlSubmenus();
+$.fn.hideCssSubmenus();
+$.fn.hideJsSubmenus();
