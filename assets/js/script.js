ebc57e33f17b382d19606f564b39e616860da50c
LukaszPasich <lukas.zed81@gmail.com>
2021-12-17 17:05:59 +0000

    FEAT: Make dropdown submenu for HTML button responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,19 @@
 $('#collapsable-navbar-html').hide();
+$('#collapsable-navbar-html-mobile').hide();
 
 $('#nav-button-html').on('click', function() {
-    if ( $('#collapsable-navbar-html').is(':hidden') ) {
-        $('#collapsable-navbar-html').slideDown(200);
-      } else {
-        $('#collapsable-navbar-html').hide(200);
-      }
-});
+    if ($(window).width() > 991.98) {
+        if ( $('#collapsable-navbar-html').is(':hidden') ) {
+            $('#collapsable-navbar-html').slideDown(200);
+        } else {
+            $('#collapsable-navbar-html').hide(200);
+        }
+    } else {
+        if ( $('#collapsable-navbar-html-mobile').is(':hidden') ) {
+            $('#collapsable-navbar-html-mobile').slideDown(200);
+        } else {
+            $('#collapsable-navbar-html-mobile').hide(200);
+        }
+}});
+
 
